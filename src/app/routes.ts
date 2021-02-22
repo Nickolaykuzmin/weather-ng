3134b41b4dca56a40ad9f5bba192b26221fbb0c8
Nickolay <kuzmin_nickolay@mail.ru>
2021-02-22 21:31:31 +0200

    lazy loading route.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,7 +3,7 @@ import { ForecastComponent } from './pages/forecast/forecast.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: 'forecast/:zipcode', component: ForecastComponent },
+  { path: 'forecast', loadChildren: () => import('./pages/forecast/forecast.module').then(r => r.ForecastModule) },
   { path: '', component: HomeComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
